1fd248008a75ed9d9a6efe19d3516eb9a73f422d
unknown <dario.patt2@gmail.com>
2021-11-18 17:00:53 +0100

    added c++ quicksort

---
+++ c++/sortingAlgorithms/quicksort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int l = (low - 1);
+
+    for (int pos = low; pos < high; pos++) {
+        if (arr[pos] <= pivot) {
+            l++;
+            swap(&arr[l], &arr[pos]);
+        }
+    }
+    
+    swap(&arr[l+1], &arr[high]);
+
+    return (l + 1);
+}
+
+void quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = partition(arr, low, high);
+
+        quicksort(arr, pivot+1, high);
+        quicksort(arr, low, high-1);
+    }
+}
